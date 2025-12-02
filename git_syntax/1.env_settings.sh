fdbcf1d3156fab5239b2e31c419ea063fad13285
ybrocks <chokanghyun1@naver.com>
2025-12-02 16:14:53 +0900

    123

--- git_syntax/1.env_settings.sh
+++ git_syntax/1.env_settings.sh
@@ -37,3 +37,8 @@ git init
 git remote add origin 원격지주소
 git checkout -b main #main브랜치 생성 및 변경
 # git add, git commit, git push 작업 진행
+
+# .gitignore파일은 git 추적목록에서 제외 대상 나열
+# 주로 빌드된 파일 또는 중요암호 등이 담긴 파일을 제외
+# 만약 이미 git에서 추적되고 있는 파일을 제외하려면 캐시 삭제 필요
+git rm -r --cached .
